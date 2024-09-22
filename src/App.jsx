dff6603f5448af83c846ec31b0f350324d9e03b2
Dazai <sanchezdanielsd@gmail.com>
2024-09-21 20:08:38 -0400

    image bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,29 @@ import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import ProjectDetail from './components/ProjectDetail';
-import bg from './assets/home-bg.jpg'
+import bg from './assets/home-bg.jpg';
 
 function App() {
   return (
-    <Router basename="/Mi-Portafolio"> {/* Definir basename aquí */}
+    <Router basename="/Mi-Portafolio">
       <div className="background" style={{
         position: 'fixed',
         top: 0,
         left: 0,
         width: '100%',
         height: '100%',
-        backgroundColor: 'blue',
+        backgroundImage: `url(${bg})`, // Asegúrate de usar backticks aquí
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         zIndex: -1,
       }}></div>
-      <Header /> {/* Siempre presente */}
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Ruta para la página principal */}
-        <Route path="/about" element={<AboutMe />} /> {/* Ruta para la página "Sobre mí" */}
-        <Route path="/projects" element={<Projects />} /> {/* Ruta para la página de proyectos */}
-        <Route path="/project/:id" element={<ProjectDetail />} /> {/* Detalle del proyecto */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
       </Routes>
     </Router>
   );
