52fb19885f64b122b22ad8f2af9fc46c17f57de1
Dazai <sanchezdanielsd@gmail.com>
2025-10-08 16:18:00 -0400

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
-import Contact from './components/Contact'
+import Contact from './components/Contact';
 import ProjectDetail from './components/ProjectDetail';
 import bg from './assets/home-bg.jpg';
 
 function App() {
   return (
-    <Router basename="/Mi-Portafolio">
-      <div className="background" style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundImage: `url(${bg})`, // Asegúrate de usar backticks aquí
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        zIndex: -1,
-      }}></div>
+    <Router>
+      <div
+        className="background"
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          backgroundImage: `url(${bg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          zIndex: -1,
+        }}
+      ></div>
+
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutMe />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
